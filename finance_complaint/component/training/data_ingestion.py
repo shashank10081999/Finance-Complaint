2b4a81fdc38ed87da477b87c248a6b98743afe9a
shashank <shashank25225@gmail.com>
2022-12-09 13:02:49 +0530

    done with data ingestion

--- finance_complaint/component/training/data_ingestion.py
+++ finance_complaint/component/training/data_ingestion.py
@@ -14,6 +14,7 @@ from finance_complaint.config.pipeline.training import FinanceConfig
 from finance_complaint.entity.artifact_entity import DataIngestionArtifact
 from finance_complaint.entity.config_entity import DataIngestionConfig
 from finance_complaint.entity.metadata_entity import DataIngestionMetadata
+from finance_complaint.config.spark_manager import spark_session
 
 from datetime import datetime
 
@@ -62,7 +63,178 @@ class DataIngestion():
         return interval
     
 
-    def download_files():
-        pass
+    def download_files(self):
+
+        """
+        n_month_interval_url: if not provided then information default value will be set
+        =======================================================================================
+        returns: List of DownloadUrl = namedtuple("DownloadUrl", ["url", "file_path", "n_retry"])
+        """
+
+        try:
+            interval = self.get_required_interval()
+
+            for index in range(1,len(interval)):
+                from_date , to_date = interval[index-1] , interval[index]
+
+                download_url : str = self.data_ingestion_config.datasource_url
+                url : str = download_url.replace("<to_date>" , to_date).replace("<from_date>" , from_date)
+                file_name : str = f"{self.data_ingestion_config.file_name}_{from_date}_{to_date}.json"
+                file_path : str = os.path.join(self.data_ingestion_config.download_dir,file_name)
+                download_url = DownloadUrl(url = url , file_path=file_path , n_retry=self.n_retry)
+                self.download_data(download_url = download_url)
+        except Exception as e:
+            raise e 
+
+    def download_data(self,download_url):
+        try:
+            download_dir = os.path.dirname(download_url.file_path)
+            os.makedirs(download_dir)
+
+            data = requests.get(download_url.url, params={'User-agent': f'your bot {uuid.uuid4()}'})
+
+            try:
+
+                with open(download_url.file_path , "w") as file_obj:
+                    finance_complaint_data = list(map(lambda x:x["_source"]) , filter(lambda x: "_source" in x.keys() , data.content))
+
+                    json.dump(finance_complaint_data , file_obj)
+
+            except Exception as e:
+
+                if os.path.exists(download_dir.file_path):
+                    os.remove(download_url.file_path)
+                self.retry_download_data(data, download_url=download_url)
+
+        except Exception as e:
+            raise e
+
+
+    def retry_download_data(self,data,download_url):
+        """
+        This function help to avoid failure as it help to download failed file again
+        
+        data:failed response
+        download_url: DownloadUrl
+        """
+        try:
+            if download_url.n_retry == 0:
+                self.failed_urls.append(download_url.url)
+                return 
+        
+            content = data.content.decode("utf-8")
+            wait_second = re.findall(r'\d+', content)
+
+            if wait_second>0:
+                time.sleep(int(wait_second[0])+2)
+            
+            failed_file_path = os.path.join(self.data_ingestion_config.failed_dir,os.path.basename(download_url.file_path))
+
+            os.makedirs(os.path.dirname(self.data_ingestion_config.failed_dir) , exist_ok=True)
+
+            with open(failed_file_path, "wb") as failed_file_obj:
+                failed_file_obj.write(data.content)
+            
+            download_url = DownloadUrl(url = download_url.url , file_path= download_url.file_path , n_retry=download_url.n_retry - 1)
+
+            self.download_data(download_url=download_url)
+
+        except Exception as e:
+            raise e 
+
+    def convert_files_to_parquet(self) -> str:
+        """
+        downloaded files will be converted and merged into single parquet file
+        json_data_dir: downloaded json file directory
+        data_dir: converted and combined file will be generated in data_dir
+        output_file_name: output file name 
+        =======================================================================================
+        returns output_file_path
+        """
+
+        try:
+            json_data_dir = self.data_ingestion_config.download_dir
+            data_dir = self.data_ingestion_config.feature_store_dir
+            output_file_name = self.data_ingestion_config.file_name
+
+            os.makedirs(data_dir,exist_ok=True)
+
+            file_path = os.path.join(data_dir, f"{output_file_name}")
+
+            if not os.path.exists(json_data_dir):
+                return file_path
+
+            for file_name in os.listdir(json_data_dir):
+                json_file_path = os.path.join(json_data_dir,file_name)
+
+                df = spark_session.read.json(json_file_path)
+                if df.count() > 0:
+                    df.write.mode('append').parquet(file_path)
+            return file_path
+        except Exception as e:
+            raise e
+        
+
+    def write_metadata(self, file_path: str) -> None:
+        """
+        This function help us to update metadata information 
+        so that we can avoid redundant download and merging.
+
+        """
+
+        try:
+
+            metadata_info = DataIngestionMetadata(metadata_file_path=self.data_ingestion_config.metadata_file_path)
+
+            metadata_info.write_metadata_info(from_date= self.data_ingestion_config.form_date , to_date=self.data_ingestion_config.to_data,data_file_path=file_path)
+
+        except Exception as e:
+            raise e 
+
+
+
+
+    def initiate_data_ingestion(self) -> DataIngestionArtifact:
+
+        try:
+
+
+            if self.data_ingestion_config.form_date != self.data_ingestion_config.to_data:
+                self.download_files()
+
+            if os.path.exists(self.data_ingestion_config.download_dir):
+                file_path = self.convert_files_to_parquet()
+                self.write_metadata(file_path=file_path)
+
+            feature_store_file_path = os.path.join(self.data_ingestion_config.feature_store_dir,self.data_ingestion_config.file_name)
+
+            artifact = DataIngestionArtifact(
+                    feature_store_file_path=feature_store_file_path,
+                    download_dir=self.data_ingestion_config.download_dir,
+                    metadata_file_path=self.data_ingestion_config.metadata_file_path)
+        
+        except Exception as e:
+            raise e 
+
+def main():
+    try:
+        config = FinanceConfig()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config,n_retry=6)
+        data_ingestion.initiate_data_ingestion()
+    except Exception as e:
+        raise e
+
+
+if __name__ == "__main__":
+    try:
+        main()
+
+    except Exception as e:
+        raise e 
+
+        
+        
+
 
 
